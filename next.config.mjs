1b27b7d263af000ce0a929720cf70ecea37edc50
Aymen <aymeneloulinti@gmail.com>
2026-09-02 16:33:47 +0200

    feat: update security headers in next.config.mjs for enhanced protection

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,14 @@ const nextConfig = {
         source: '/:path*',
         headers: [
           { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' http://localhost:3000",
+          },
           {
             key: 'Permissions-Policy',
             value: 'camera=(), microphone=(), geolocation=()',
