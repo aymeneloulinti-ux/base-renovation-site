3accab641dea13d6ca90715c5600e006a64f3d62
Aymen <aymeneloulinti@gmail.com>
2026-09-07 12:00:02 +0200

    feat: implement headers configuration in next.config.ts for enhanced security

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
 
-export default nextConfig;
+  async headers() {
+
+    return [
+
+      {
+
+        source: '/:path*',
+
+        headers: [
+
+          {
+
+            key: 'Content-Security-Policy',
+
+            value: "frame-ancestors 'self' https://web-agency-nine-kohl.vercel.app",
+
+          },
+
+        ],
+
+      },
+
+    ]
+
+  },
+
+}
+
+export default nextConfig
